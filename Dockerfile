e3490222003a6b8f0958a609a84abd289dc7283a
m1bcodes <m1bcodes@web.de>
2024-06-14 22:05:23 +0000

    added HD movies

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,35 @@
 # syntax=docker/dockerfile:1
-FROM python:3.9
+FROM python:latest
 WORKDIR /app
 
-RUN apt-get update && apt-get upgrade && apt-get install -y ffmpeg
+RUN echo "hello there"
+
+RUN apt-get update && apt-get install -y ffmpeg
 RUN apt-get -y install cron
-RUN git clone https://github.com/m1bcodes/ytdlwl-docker.git
+
+
+# RUN git clone https://github.com/m1bcodes/ytdlwl-docker.git
+
 RUN python3 -m pip install --upgrade yt-dlp
 COPY . .
 
 COPY ytdlwl-cron /etc/cron.d/ytdlwl-cron
+
 # Give execution rights on the cron job
 RUN chmod 0644 /etc/cron.d/ytdlwl-cron
+
 # Apply cron job
 RUN crontab /etc/cron.d/ytdlwl-cron
+
 # Create the log file to be able to run tail
 RUN touch /var/log/cron.log
+
+RUN chmod a+x update_wl.sh
+
 # Run the command on container startup
 CMD cron && tail -f /var/log/cron.log
+
+# ...or: run the update immediately
 # CMD sh update_wl.sh
+
+RUN echo "end 23"
--- update_wl.sh
+++ update_wl.sh
@@ -4,16 +4,29 @@ echo "****************************************"
 echo "*** Starting $(date)"
 echo "****************************************"
 
-OUTPUT_PATH=./downloads
+PLAYLIST_FILE=/var/tmp/playlist.json
+VIDEO_PATH=/var/Videos/Youtube_WL
+VIDEO_PATH_HD=/var/Videos/Youtube_WL_HD
+COOKIES_FILE=/run/secrets/yt-cookies
 
 echo downloading playlist...
-yt-dlp -J --cookies ./cookies/cookies.txt https://www.youtube.com/playlist?list=WL > ${OUTPUT_PATH}/playlist.json
+yt-dlp -J --cookies ${COOKIES_FILE} https://www.youtube.com/playlist?list=WL > ${PLAYLIST_FILE}
+
+mkdir -p ${VIDEO_PATH_HD}
+mkdir -p ${VIDEO_PATH_HD}
 
 echo purging...
-python3 remove_videos_not_in_playlist.py ${OUTPUT_PATH} ${OUTPUT_PATH}/playlist.json
+python3 remove_videos_not_in_playlist.py ${VIDEO_PATH} ${PLAYLIST_FILE}
+python3 remove_videos_not_in_playlist.py ${VIDEO_PATH_HD} ${PLAYLIST_FILE}
 
 echo downloading files...
-yt-dlp --embed-thumbnail --embed-metadata --embed-chapters --restrict-filenames -f "best[height=720]" --cookies ./cookies/cookies.txt --paths $OUTPUT_PATH https://www.youtube.com/playlist?list=WL 
+yt-dlp --download-archive ${VIDEO_PATH}/archive.txt --embed-thumbnail --embed-metadata --embed-chapters --restrict-filenames -f "best[height=720]" --cookies ${COOKIES_FILE} --paths $VIDEO_PATH https://www.youtube.com/playlist?list=WL 
+yt-dlp --download-archive ${VIDEO_PATH_HD}/archive.txt --embed-thumbnail --embed-metadata --embed-chapters --restrict-filenames -f "bv*[height<=1080]+ba/b[height<=1080]/wv*+ba/w" --cookies ${COOKIES_FILE} --paths $VIDEO_PATH_HD https://www.youtube.com/playlist?list=WL 
 
 echo adjusting file dates...
-python3 set_file_date.py ${OUTPUT_PATH} ${OUTPUT_PATH}/playlist.json
+python3 set_file_date.py ${VIDEO_PATH} ${PLAYLIST_FILE}
+python3 set_file_date.py ${VIDEO_PATH_HD} ${PLAYLIST_FILE}
+
+echo "****************************************"
+echo "*** Completed $(date)"
+echo "****************************************"
